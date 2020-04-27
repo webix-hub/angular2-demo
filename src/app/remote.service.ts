d9e9dc5d5f9885f822ef50826533cbfd516919f8
olistopad <olistopad@xbsoftware.com>
2020-04-27 17:36:58 +0300

    External data source: myjson => jsonbin

--- src/app/remote.service.ts
+++ src/app/remote.service.ts
@@ -7,7 +7,7 @@ export class RemoteDataService {
   constructor(private http: HttpClient) { }
 
   getFilms():Promise<Film[]>{
-    return this.http.get<Film[]>(`https://api.myjson.com/bins/12aesl`).toPromise();
+    return this.http.get<Film[]>(`https://api.jsonbin.io/b/5ea6df0b98b3d537523566e7`).toPromise();
   }
 
 }
